95d9da290b44eccd183cb2fe410f1891e507034d
Mayur Suvarna <97018632+Mayur2728@users.noreply.github.com>
2022-03-14 06:14:43 +0000

    1,3,4,5

--- p1original.c
+++ p1original.c
@@ -1,2 +1,33 @@
-
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter the num 1 and den 1\n");
+  scanf("%d%d",num1,den1);
+  printf("enter the num 2 and den 2\n");
+  scanf("%d%d",num2, den2);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  *den3=den1*den2;
+  *num3=(num1*den2)+(num2*den1);
+  for (int i=1;i<=*den3;i++)
+  {
+      if(*den3%i ==0 && *num3%i ==0)
+      {
+          *den3=*den3/i;
+          *num3=*num3/i;
+      }
+  }
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+    printf("The addition of %d/%d and %d/%d = %d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
 }
---
+++ p3original.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void input(int *n, int *r)
+{
+  printf("Enter the values of 'n' and 'r'\n");
+  scanf("%d%d",n,r);
+}
+int ncr(int n , int r)
+{
+  int result=1;
+  for(int i=1;i<=r;i++)
+    {
+      result=result*(n-i+1)/i;
+    }
+  return result;
+}
+void output(int n , int r, int result)
+{
+  printf("The ncr value of %d and %d is %d\n",n,r,result);
+}
+int main()
+{
+  int n,r,result;
+  input(&n,&r);
+  result=ncr(n,r);
+  output(n,r,result);
+  return 0;
+}
+
